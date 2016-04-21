271407c9b1b4470c3002c19a33dfaf6eb99f6132
Savad KP <savadppns@gmail.com>
2016-04-21 12:54:08 +0530

    Initial to heroku

--- pin/settings.py
+++ pin/settings.py
@@ -105,7 +105,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "staticfiles"),
+]
 STATIC_URL = '/static/'
 
 HAYSTACK_SEARCH_RESULTS_PER_PAGE = 12
